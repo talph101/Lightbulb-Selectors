e94a293444a91504cb6b10d1739332ffd2dd9f0a
Tiffany <139176913+talph101@users.noreply.github.com>
2024-09-09 09:19:03 -0500

    Added variables and event listener to JS file.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Write your code here
+// if active lightbulb on
+let select1 = document.querySelector("#lightbulb1");
+let select2 = document.querySelector("#lightbulb2");
+let select3 = document.querySelector("#lightbulb3");
+
+let title = document.querySelector(".subtitle")
+
+let count = 0;
+
+select1.addEventListener("click", function(){
+    count++;
+    title = "You've clicked the lights "+ count+" times"
+
+    select1.classList.toggle(".item.lightbulb.active");
+
+}) 
+
